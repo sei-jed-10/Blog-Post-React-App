c7ac6d14cbedb05c468b388fbf27c91bc01ba43f
saadkhan29 <57666391+saadkhan29@users.noreply.github.com>
2020-01-08 15:32:22 +0300

    Updates

---
+++ precache-manifest.f0f0ce4a8aa136ed2e7a32bf15da40d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b1be297b878111c33e4af30b92da7a",
+    "url": "/Blog-Post-React-App/index.html"
+  },
+  {
+    "revision": "8f04244dad2bba850cf4",
+    "url": "/Blog-Post-React-App/static/css/main.698f4fa5.chunk.css"
+  },
+  {
+    "revision": "9ce22423c9d075639bb4",
+    "url": "/Blog-Post-React-App/static/js/2.1c799c67.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Blog-Post-React-App/static/js/2.1c799c67.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f04244dad2bba850cf4",
+    "url": "/Blog-Post-React-App/static/js/main.efa325a3.chunk.js"
+  },
+  {
+    "revision": "70abe5dd47babe763d87",
+    "url": "/Blog-Post-React-App/static/js/runtime-main.e39e698e.js"
+  }
+]);
